ad1a20183f360e19870fcd3b6b96e724ed4680b6
Juan-Carlos Barros <70406308+juancarlosbarros@users.noreply.github.com>
2022-04-18 00:16:47 +0200

    finish Day14

--- Day14.py
+++ Day14.py
@@ -17,8 +17,6 @@ total_time = 2503
 
 
 # part1
-
-
 def distance(total_time, speed, flight_time, rest_time):
     step_time = flight_time + rest_time
     steps = total_time // step_time
@@ -30,11 +28,11 @@ def part1():
     for animal in datas:
         distances[animal[0]] = distance(total_time, animal[1], animal[2], animal[3])
     print(max(distances), distances)
-    
- 
+
+
 # part2
 def part2():
-    tot_time = 50
+    tot_time = 2503
     distances = {animal[0]:[0] for animal in datas}
     # print(distances)
     for animal in datas:
@@ -43,26 +41,29 @@ def part2():
         speed = animal[1]
         flight_time = animal[2]
         rest_time = animal[3]
-        while time < tot_time:
-            steps = min(flight_time, tot_time-flight_time)
+        while time < tot_time+1:
+            steps = min(flight_time, tot_time+1-flight_time)
             for _ in range(steps):
                 time += 1
                 dist += speed
                 distances[animal[0]].append(dist)
-            steps = min(tot_time-time, rest_time)
+            steps = min(tot_time+1-time, rest_time)
             distances[animal[0]] += steps*[dist]
             time += steps
     # print(distances)
-    reverse_distances = [{animal[0]:distances[animal[0]][i] for animal in datas} for i in range(tot_time)]
-    print(distances, reverse_distances)
+    reverse_distances = [{animal[0]:distances[animal[0]][i] for animal in datas} for i in range(tot_time+1)]
+    # print(distances, reverse_distances)
     points = {animal[0]:0 for animal in datas}
     for i in range(1, tot_time+1):
+        # print("\n", i, "\t", end="")
         max_distance = max(reverse_distances[i].values())
         for key, val in reverse_distances[i].items():
             if val == max_distance:
+                # print(key, points[key], end = '\t')
                 points[key] += 1
-    print(points)
-        
+    # print(points)
+    print("part2: ", max(points.values()))
+
 
 
 # part1()
